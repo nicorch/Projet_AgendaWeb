6160bdedfbaad9cd8f6c164336db26ed9d3eda96
NicolasRoche <nicolas8roche@gmail.com>
2020-03-19 10:37:42 +0100

    Cours Formulaire (pas dans admin): choix minute 0 ou 30

--- src/Form/CoursType.php
+++ src/Form/CoursType.php
@@ -6,14 +6,19 @@ use App\Entity\Cours;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class CoursType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('dateHeureDebut')
-            ->add('dateHeureFin')
+            ->add('dateHeureDebut',DateTimeType::class, [
+                'minutes' => [00,30]
+            ])
+            ->add('dateHeureFin',DateTimeType::class, [
+                'minutes' => [00,30]
+            ])
             ->add('type')
             ->add('professeur')
             ->add('salle')
